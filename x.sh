27e2539c0e6fe130b1d62d76e26a08c133e33b1b
fatmi9 <sitinurfatmi7@gmail.com>
2024-11-23 06:18:28 +0700

    Create x.sh

---
+++ x.sh
@@ -0,0 +1,6 @@
+apt update && apt -y install php php-curl ca-certificates nano python3 python3-pip && pip3 install requests
+pip3 install asyncio
+pip3 install loguru
+pip3 install websockets_proxy
+pip3 install fake_useragent
+pip3 install websockets
